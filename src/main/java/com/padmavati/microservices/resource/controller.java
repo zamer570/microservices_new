6da1fef8763c4543dc1094d9fb2461fb2a52bdb6
apple <zamersbouy@gmail.com>
2023-10-16 21:57:47 +0530

    excel api

--- src/main/java/com/padmavati/microservices/resource/controller.java
+++ src/main/java/com/padmavati/microservices/resource/controller.java
@@ -5,11 +5,17 @@ import com.padmavati.microservices.enitity.User;
 import com.padmavati.microservices.model.request.UserRequest;
 import com.padmavati.microservices.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -41,6 +47,29 @@ public class controller {
     public ResponseEntity<String> createUser(@RequestBody UserRequest user) {
         String response = userService.addUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+
+    @GetMapping("/export-users")
+    public ResponseEntity<Resource> exportUsers() {
+        ByteArrayInputStream file = null;
+        try {
+             file = userService.generateExcelForUsers();
+        }
+        catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+//        return ResponseEntity.ok()
+//                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=data.xlsx")
+//                .body(file);
+        String filename = "data.xlsx";
+        InputStreamResource files = new InputStreamResource(file);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + filename)
+                .contentType(MediaType.parseMediaType("application/vnd.ms-excel"))
+                .body(files);
+
     }
 
     //request mapping
--- src/main/java/com/padmavati/microservices/service/UserService.java
+++ src/main/java/com/padmavati/microservices/service/UserService.java
@@ -4,9 +4,18 @@ import com.padmavati.microservices.enitity.User;
 import com.padmavati.microservices.exception.ResourceNotFoundException;
 import com.padmavati.microservices.model.request.UserRequest;
 import com.padmavati.microservices.repository.UserRepository;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -35,4 +44,39 @@ public class UserService {
         return "User added successfully";
     }
 
+    public ByteArrayInputStream generateExcelForUsers() throws IOException {
+        List<User> users = getAllUsers();
+        Workbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet("users");
+
+        Row row = sheet.createRow(0);
+        List<String> headers = Arrays.asList("Id","Name","Age","Designation");
+        for(int i=0;i<4;i++) {
+            Cell cell = row.createCell(i);
+            cell.setCellValue(headers.get(i));
+        }
+        int rowCounter=1;
+        for(int i=0;i< users.size();i++){
+            User user = users.get(i);
+            Row userRow = sheet.createRow(rowCounter++);
+            int cellCounter = 0;
+            //id
+            Cell cell = userRow.createCell(cellCounter++);
+            cell.setCellValue(user.getId());
+
+            //name
+            cell = userRow.createCell(cellCounter++);
+            cell.setCellValue(user.getName());
+
+            //age
+            userRow.createCell(cellCounter++).setCellValue(user.getAge());
+
+            //designation
+            cell = userRow.createCell(cellCounter++);
+            cell.setCellValue(user.getDesignation());
+        }
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        workbook.write(outputStream);
+        return new ByteArrayInputStream(outputStream.toByteArray());
+    }
 }
