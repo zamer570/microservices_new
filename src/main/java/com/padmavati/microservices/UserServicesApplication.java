54d8e84c6099b08241b63a1e299c657725a929d5
apple <zamersbouy@gmail.com>
2023-10-18 22:25:24 +0530

    change in user

--- src/main/java/com/padmavati/microservices/UserServicesApplication.java
+++ src/main/java/com/padmavati/microservices/UserServicesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MicroservicesProjectApplication {
+public class UserServicesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroservicesProjectApplication.class, args);
+		SpringApplication.run(UserServicesApplication.class, args);
 	}
 
 }
